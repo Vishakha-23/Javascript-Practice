48c6e82855f135e6768c8d0b13c9aa8a49a8eec1
Vishakha Baghel <163894187+Vishakha-23@users.noreply.github.com>
2025-06-14 20:16:43 +0530

    more on loops

--- for-loop-practice.js
+++ for-loop-practice.js
@@ -1,19 +1,43 @@
- //initializing object
- const obj = {
-    'name' : 'Vishakha',
-    'role' : 'student',
-    'skill': 'Web development'
- }
+//initializing object
+const obj = {
+  'name': 'Vishakha',
+  'role': 'student',
+  'skill': 'Web development'
+}
 
- //for-in loop : loop through keys of an object
- for (const key in obj) {
+//for-in loop : loop through keys of an object
+for (const key in obj) {
+
+  const element = obj[key];
+  console.log(element);
+
+}
+
+//for-of loop : loop through iterable data structure
+for (const element of 'Vishakha') {
+  console.log(element);
+}
+
+//write a program to print the marks of student in object below
+let obj1 = {
+  'harry': 98,
+  'rohan': 70,
+  'akash': 7
+}
+
+//returns array of key vlue pair of object
+console.log(Object.entries(obj1));
+
+const obj_arr = Object.entries(obj1);
+for (let i = 0; i < obj_arr.length; i++) {
   
-      const element = obj[key];
-      console.log(element);
-   
- }
+  console.log(obj_arr[i]);
+}
 
- //for-of loop : loop through iterable data structure
- for (const element of 'Vishakha') {
-   console.log(element);
- }
+
+for (const key in obj1) {
+  if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+    const element = obj1[key];//to print values
+    console.log(key);//to print keys
+  }
+}
---
+++ function-practice.js
@@ -0,0 +1,11 @@
+//function
+function myfunction(name, age) {
+    console.log(`my name is ${name} and I am ${age} years old. `)
+}
+myfunction('vishakha', '20');
+
+//arrow function
+let func1 = (x, y) => {
+    console.log(x + y);
+}
+func1(5, 6);
