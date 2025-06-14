148d68de9522cccb9d678640212d2c35d117a260
Vishakha Baghel <163894187+Vishakha-23@users.noreply.github.com>
2025-06-14 20:50:59 +0530

    initial commit

---
+++ string-practice.js
@@ -0,0 +1,20 @@
+
+let name1 = 'Vishakha';
+let name2 = 'Nidhi';
+
+//string methods
+
+console.log(name1.length);
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.trim());
+console.log(name1.slice(1,5));
+console.log(name1.slice(2));
+console.log(name1.concat(name2));
+console.log(name1.concat(name2, ' Hello'));
+console.log(name1[0]);
+console.log('name\''.length);
+console.log(name1.includes(5))
+console.log(name1.startsWith(5))
+console.log(name1.endsWith(5))
+
